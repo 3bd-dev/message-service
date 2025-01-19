8b4ca10cbc860e30b33edc71c78003510dca620c
Abdalrahman Saadulla <abdalrahmansaadulla@gmail.com>
2025-01-19 23:53:01 +0300

    update seed

--- internal/db/seed/seed.sql
+++ internal/db/seed/seed.sql
@@ -1,11 +1,11 @@
 INSERT INTO users (id, username, role) VALUES
-    (uuid_generate_v4(), 'checker1', 'checker')
+    ('550e8400-e29b-41d4-a716-446655440000', 'maker1', 'maker')
 ON CONFLICT (username) DO NOTHING;
 
 INSERT INTO users (id, username, role) VALUES
-    (uuid_generate_v4(), 'checker2', 'checker')
+    ('550e8400-e29b-41d4-a716-446655440001', 'checker1', 'checker')
 ON CONFLICT (username) DO NOTHING;
 
 INSERT INTO users (id, username, role) VALUES
-    (uuid_generate_v4(), 'maker1', 'maker')
+    ('550e8400-e29b-41d4-a716-446655440002', 'checker2', 'checker')
 ON CONFLICT (username) DO NOTHING;
